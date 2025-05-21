c9ef24dd7149156c234c140177c7e4007367bdd8
Elton Marques <elton1314@hotmail.com>
2025-05-20 21:16:12 -0300

    feat(backend): add Redis Module for Cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { LoggerModule } from 'nestjs-pino';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
 
 @Module({
   imports: [
@@ -15,6 +16,13 @@ import { LoggerModule } from 'nestjs-pino';
       ],
     }),
     LoggerModule.forRoot(),
+    RedisModule.forRoot({
+      config: {
+        host: '0.0.0.0',
+        port: 6379,
+        password: 'password',
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
